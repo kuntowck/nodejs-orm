c1873712677a631cb9f7de3ba1a4fc4bd7fd10a7
kuntowck <kunto312002@gmail.com>
2024-07-04 17:30:42 +0700

    updateMany data customers

--- tests/crud-many.test.js
+++ tests/crud-many.test.js
@@ -2,24 +2,40 @@ import { prismaClient } from "../src/prisma-client";
 
 /* eslint-disable no-undef */
 describe("prisma client", () => {
-  it("should can create many records", async () => {
-    const { count } = await prismaClient.customer.createMany({
-      data: [
-        {
-          id: "reyhan05",
-          name: "reyhan siddiq",
-          email: "reyhansiddiq@gmail.com",
-          phone: "08912416931",
-        },
-        {
-          id: "faiz06",
-          name: "faiz mecca",
-          email: "faizmecca@gmail.com",
-          phone: "087612193112",
-        },
-      ],
+  // it("should can create many records", async () => {
+  //   const { count } = await prismaClient.customer.createMany({
+  //     data: [
+  //       {
+  //         id: "reyhan05",
+  //         name: "reyhan siddiq",
+  //         email: "reyhansiddiq@gmail.com",
+  //         phone: "08912416931",
+  //       },
+  //       {
+  //         id: "faiz06",
+  //         name: "faiz mecca",
+  //         email: "faizmecca@gmail.com",
+  //         phone: "087612193112",
+  //       },
+  //     ],
+  //   });
+
+  //   expect(count).toBe(2);
+  // });
+
+  it("should can update many records", async () => {
+    // updateMany != update
+    // update: records yang primary atau unique
+    // updateMany: semua records
+    const { count } = await prismaClient.customer.updateMany({
+      data: {
+        email: "kuntowicaksono@gmail.com",
+      },
+      where: {
+        name: "kunto wicaksono",
+      },
     });
 
-    expect(count).toBe(2);
+    expect(count).toBe(1);
   });
 });
