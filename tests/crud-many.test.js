fc4edb7f84943ca424cd8d5ff178ea5da2468aca
kuntowck <kunto312002@gmail.com>
2024-07-04 17:40:42 +0700

    ready many (findMany) data customers

--- tests/crud-many.test.js
+++ tests/crud-many.test.js
@@ -2,42 +2,42 @@ import { prismaClient } from "../src/prisma-client";
 
 /* eslint-disable no-undef */
 describe("prisma client", () => {
-  // it("should can create many records", async () => {
-  //   const { count } = await prismaClient.customer.createMany({
-  //     data: [
-  //       {
-  //         id: "reyhan05",
-  //         name: "reyhan siddiq",
-  //         email: "reyhansiddiq@gmail.com",
-  //         phone: "08912416931",
-  //       },
-  //       {
-  //         id: "faiz06",
-  //         name: "faiz mecca",
-  //         email: "faizmecca@gmail.com",
-  //         phone: "087612193112",
-  //       },
-  //     ],
-  //   });
+  it("should can create many records", async () => {
+    const { count } = await prismaClient.customer.createMany({
+      data: [
+        {
+          id: "reyhan05",
+          name: "reyhan siddiq",
+          email: "reyhansiddiq@gmail.com",
+          phone: "08912416931",
+        },
+        {
+          id: "faiz06",
+          name: "faiz mecca",
+          email: "faizmecca@gmail.com",
+          phone: "087612193112",
+        },
+      ],
+    });
 
-  //   expect(count).toBe(2);
-  // });
+    expect(count).toBe(2);
+  });
 
-  // it("should can update many records", async () => {
-  //   // updateMany != update
-  //   // update: records yang primary atau unique
-  //   // updateMany: semua records
-  //   const { count } = await prismaClient.customer.updateMany({
-  //     data: {
-  //       email: "kuntowicaksono@gmail.com",
-  //     },
-  //     where: {
-  //       name: "kunto wicaksono",
-  //     },
-  //   });
+  it("should can update many records", async () => {
+    // updateMany != update
+    // update: records yang primary atau unique
+    // updateMany: semua records
+    const { count } = await prismaClient.customer.updateMany({
+      data: {
+        email: "kuntowicaksono@gmail.com",
+      },
+      where: {
+        name: "kunto wicaksono",
+      },
+    });
 
-  //   expect(count).toBe(1);
-  // });
+    expect(count).toBe(1);
+  });
 
   it("should can delete many records", async () => {
     // deleteMany != delete
@@ -50,5 +50,13 @@ describe("prisma client", () => {
     });
 
     expect(count).toBe(0);
+  });
+
+  it("should can read many records", async () => {
+    const datas = await prismaClient.customer.findMany({});
+
+    console.log(datas);
+
+    expect(datas.length).toBe(6);
   });
 });
