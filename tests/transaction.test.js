d61e557a7d57477d0dd734b0e1cc923b5fd901d3
kuntowck <kunto312002@gmail.com>
2024-07-04 15:20:51 +0700

    interactive transaction data

--- tests/transaction.test.js
+++ tests/transaction.test.js
@@ -25,4 +25,30 @@ describe("prisma client", () => {
     expect(data1.name).toBe("kunto wicaksono");
     expect(data2.name).toBe("faturrahman");
   });
+
+  it("should can execute interactive transaction", async () => {
+    const [data1, data2] = await prismaClient.$transaction(async (prisma) => {
+      const data1 = await prisma.customer.create({
+        data: {
+          id: "kunto001",
+          name: "kunto wicaksono",
+          email: "kuntow@gmail.com",
+          phone: "0812913471190",
+        },
+      });
+      const data2 = await prisma.customer.create({
+        data: {
+          id: "fatur004",
+          name: "faturrahman",
+          email: "faturrhmn@gmail.com",
+          phone: "0812098861497",
+        },
+      });
+
+      return [data1, data2];
+    });
+
+    expect(data1.name).toBe("kunto wicaksono");
+    expect(data2.name).toBe("faturrahman");
+  });
 });
